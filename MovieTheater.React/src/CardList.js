e259bd011ef98face5e58ec884487ee158231108
ecarpouzis <Atoramos@Gmail.com>
2021-06-16 22:48:41 -0400

    Updated card styling

--- MovieTheater.React/src/CardList.js
+++ MovieTheater.React/src/CardList.js
@@ -2,23 +2,115 @@
 /* eslint-disable jsx-a11y/anchor-has-content */
 
 import { MovieAPI } from "./MovieAPI";
+import { Card } from "antd";
+import { Scrollbars } from "react-custom-scrollbars";
+
+const gridStyle = {
+  width: "400px",
+  height: "200px",
+  textAlign: "center",
+  margin: "10px",
+  padding: "0px",
+  display: "flex",
+};
+
+const cardPosterStyle = {
+  height: "100%",
+  float: "left",
+};
+
+const cardTitleStyle = {
+  fontWeight: "bold",
+  fontFamily: "Arial Black",
+  color: "#5E5E5E",
+  width: "100%",
+  textAlign: "left",
+  float: "left",
+  paddingLeft: "5px",
+};
+
+const cardRatingStyle = {
+  float: "left",
+  paddingLeft: "10px",
+  width: "10%",
+  fontFamily: "Georgia",
+  fontWeight: "bold",
+};
+
+const cardTimeStyle = {
+  float: "left",
+  paddingLeft: "5px",
+};
+
+const cardPlotStyle = {
+  textAlign: "left",
+  display: "block",
+  clear: "left",
+  paddingLeft: "5px",
+};
+
+const cardRightColumStyle = {
+  flexGrow: "1",
+  overflowY: "auto",
+  textAlign: "left",
+  paddingLeft: "5px",
+  paddingRight: "10px",
+};
 console.log(MovieAPI.getPosterThumbnail(100));
 
 function CardList() {
   return (
-    <div>
-      <Card
+    <Card>
+      <MovieCard
+        movieID="100"
+        movieTitle="A Brighter Summer Day (1991)"
+        movieRating="R"
+        movieTime="3 h 57 min"
+        movieActors={["Eric Carpouzis", "Neil Bostian"]}
+        moviePlot={
+          "Millions of Mainland Chinese fled to Taiwan with the National Government after its civil war defeat by the Chinese Communists in 1949. Their children were brought up in an uneasy atmosphere created by the parents' own uncertainty about the future. Many formed street gangs to search for identity and to strengthen their sense of security."
+        }
+      ></MovieCard>
+      <MovieCard
         movieID="100"
         movieTitle="TestMovie"
         movieRating="R"
         movieTime="1h 30m"
         movieActors={["Eric Carpouzis", "Neil Bostian"]}
-      ></Card>
-    </div>
+      ></MovieCard>
+      <MovieCard
+        movieID="100"
+        movieTitle="TestMovie"
+        movieRating="R"
+        movieTime="1h 30m"
+        movieActors={["Eric Carpouzis", "Neil Bostian"]}
+      ></MovieCard>
+      <MovieCard
+        movieID="100"
+        movieTitle="TestMovie"
+        movieRating="R"
+        movieTime="1h 30m"
+        movieActors={["Eric Carpouzis", "Neil Bostian"]}
+      ></MovieCard>
+      <MovieCard
+        movieID="100"
+        movieTitle="TestMovie"
+        movieRating="R"
+        movieTime="1h 30m"
+        movieActors={["Eric Carpouzis", "Neil Bostian"]}
+      ></MovieCard>
+    </Card>
   );
 }
 
-function Card({ movieID, movieTitle, movieRating, movieTime, movieActors }) {
+function MovieCard({
+  movieID,
+  movieTitle,
+  movieRating,
+  movieTime,
+  movieActors,
+  moviePlot,
+}) {
   const thumbUrl = MovieAPI.getPosterThumbnail(movieID);
 
   const actorList = movieActors.map((actor) => (
@@ -28,20 +120,46 @@ function Card({ movieID, movieTitle, movieRating, movieTime, movieActors }) {
     </div>
   ));
 
+  const cardActorSpacer = {
+    width: "40%",
+    float: "left",
+    textAlign: "left",
+    paddingLeft: "5px",
+  };
+
   return (
-    <div>
-      <img class="moviePosterImage" alt="" src={thumbUrl} />
-      <a href="#" class="movieTitle">
-        {movieTitle}
-      </a>
-      <span class="movieRating">{movieRating}</span>
-      <br />
-      <span class="movieTime">{movieTime}</span>
-      {actorList}
-      <div class="actorSpacer"></div>
-      <div class="actorSpacer"></div>
-      <span class="moviePlot"></span>
-    </div>
+    <Card.Grid style={gridStyle}>
+      <div>
+        <img
+          class="moviePosterImage"
+          style={cardPosterStyle}
+          alt=""
+          src={thumbUrl}
+        />
+      </div>
+      <Scrollbars autoHide>
+        <div style={cardRightColumStyle}>
+          <a href="#" style={cardTitleStyle} class="movieTitle">
+            {movieTitle}
+          </a>
+          <br />
+          <span class="movieTime" style={cardTimeStyle}>
+            {movieTime}
+          </span>
+          <span class="movieRating" style={cardRatingStyle}>
+            {movieRating}
+          </span>
+          <br />
+          <div style={cardActorSpacer}>{actorList}</div>
+
+          <div class="actorSpacer"></div>
+          <div class="actorSpacer"></div>
+          <span class="moviePlot" style={cardPlotStyle}>
+            {moviePlot}
+          </span>
+        </div>
+      </Scrollbars>
+    </Card.Grid>
   );
 }
 
