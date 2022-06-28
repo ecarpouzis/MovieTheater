39b0d97f367a759bca38266057b98e4b3c0c1f1c
Neil Bostian <neil.bostian@gmail.com>
2022-06-27 21:52:09 -0400

    chore: readiness probe

---
+++ src/MovieTheater/Controllers/MainController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MovieTheater.Controllers
+{
+    public class MainController : ControllerBase
+    {
+        [HttpGet("/api/status")]
+        public ActionResult Status()
+        {
+            return Ok();
+        }
+    }
+}
