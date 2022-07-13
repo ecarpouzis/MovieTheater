9c3e7265d1f5cfd496e128f393fad42c3814b3dd
Neil Bostian <neil.bostian@gmail.com>
2022-07-13 14:33:32 -0400

    chore: organize things

--- src/ui/src/InsertPage.js
+++ src/ui/src/InsertPage.js
@@ -13,13 +13,7 @@ function InsertMovieInput({ placeholder, name, movieState, setMovieState }) {
     setMovieState(newState);
   }
 
-  return (
-    <Input
-      placeholder={placeholder}
-      value={movieState[name]}
-      onChange={onChange}
-    />
-  );
+  return <Input placeholder={placeholder} value={movieState[name]} onChange={onChange} />;
 }
 
 function InsertPage() {
@@ -55,155 +49,85 @@ function InsertPage() {
           <tr>
             <td>Title</td>
             <td>
-              <InsertMovieInput
-                placeholder="Title"
-                name="title"
-                movieState={movieState}
-                setMovieState={setMovieState}
-              />
+              <InsertMovieInput placeholder="Title" name="title" movieState={movieState} setMovieState={setMovieState} />
             </td>
           </tr>
           <tr>
             <td>Simple Title</td>
             <td>
-              <InsertMovieInput
-                placeholder="Simple Title"
-                name="simpleTitle"
-                movieState={movieState}
-                setMovieState={setMovieState}
-              />
+              <InsertMovieInput placeholder="Simple Title" name="simpleTitle" movieState={movieState} setMovieState={setMovieState} />
             </td>
           </tr>
           <tr>
             <td>Rating</td>
             <td>
-              <InsertMovieInput
-                placeholder="Rating"
-                name="rating"
-                movieState={movieState}
-                setMovieState={setMovieState}
-              />
+              <InsertMovieInput placeholder="Rating" name="rating" movieState={movieState} setMovieState={setMovieState} />
             </td>
           </tr>
           <tr>
             <td>Release Date</td>
             <td>
-              <InsertMovieInput
-                placeholder="Release Date"
-                name="releaseDate"
-                movieState={movieState}
-                setMovieState={setMovieState}
-              />
+              <InsertMovieInput placeholder="Release Date" name="releaseDate" movieState={movieState} setMovieState={setMovieState} />
             </td>
           </tr>
           <tr>
             <td>Runtime</td>
             <td>
-              <InsertMovieInput
-                placeholder="Runtime"
-                name="runtime"
-                movieState={movieState}
-                setMovieState={setMovieState}
-              />
+              <InsertMovieInput placeholder="Runtime" name="runtime" movieState={movieState} setMovieState={setMovieState} />
             </td>
           </tr>
           <tr>
             <td>Genre</td>
             <td>
-              <InsertMovieInput
-                placeholder="Genre"
-                name="genre"
-                movieState={movieState}
-                setMovieState={setMovieState}
-              />
+              <InsertMovieInput placeholder="Genre" name="genre" movieState={movieState} setMovieState={setMovieState} />
             </td>
           </tr>
           <tr>
             <td>Director</td>
             <td>
-              <InsertMovieInput
-                placeholder="Director"
-                name="director"
-                movieState={movieState}
-                setMovieState={setMovieState}
-              />
+              <InsertMovieInput placeholder="Director" name="director" movieState={movieState} setMovieState={setMovieState} />
             </td>
           </tr>
           <tr>
             <td>Writer</td>
             <td>
-              <InsertMovieInput
-                placeholder="Writer"
-                name="writer"
-                movieState={movieState}
-                setMovieState={setMovieState}
-              />
+              <InsertMovieInput placeholder="Writer" name="writer" movieState={movieState} setMovieState={setMovieState} />
             </td>
           </tr>
           <tr>
             <td>Actors</td>
             <td>
-              <InsertMovieInput
-                placeholder="Actors"
-                name="actors"
-                movieState={movieState}
-                setMovieState={setMovieState}
-              />
+              <InsertMovieInput placeholder="Actors" name="actors" movieState={movieState} setMovieState={setMovieState} />
             </td>
           </tr>
           <tr>
             <td>Plot</td>
             <td>
-              <InsertMovieInput
-                placeholder="Plot"
-                name="plot"
-                movieState={movieState}
-                setMovieState={setMovieState}
-              />
+              <InsertMovieInput placeholder="Plot" name="plot" movieState={movieState} setMovieState={setMovieState} />
             </td>
           </tr>
           <tr>
             <td>Poster Link</td>
             <td>
-              <InsertMovieInput
-                placeholder="Poster Link"
-                name="posterLink"
-                movieState={movieState}
-                setMovieState={setMovieState}
-              />
+              <InsertMovieInput placeholder="Poster Link" name="posterLink" movieState={movieState} setMovieState={setMovieState} />
             </td>
           </tr>
           <tr>
             <td>IMDB Rating</td>
             <td>
-              <InsertMovieInput
-                placeholder="IMDB Rating"
-                name="imdbRating"
-                movieState={movieState}
-                setMovieState={setMovieState}
-              />
+              <InsertMovieInput placeholder="IMDB Rating" name="imdbRating" movieState={movieState} setMovieState={setMovieState} />
             </td>
           </tr>
           <tr>
             <td>IMDB ID</td>
             <td>
-              <InsertMovieInput
-                placeholder="IMDB ID"
-                name="imdbID"
-                movieState={movieState}
-                setMovieState={setMovieState}
-              />
+              <InsertMovieInput placeholder="IMDB ID" name="imdbID" movieState={movieState} setMovieState={setMovieState} />
             </td>
           </tr>
           <tr>
             <td>Tomato Rating</td>
             <td>
-              <InsertMovieInput
-                placeholder="Tomato Rating"
-                name="tomatoRating"
-                movieState={movieState}
-                setMovieState={setMovieState}
-              />
+              <InsertMovieInput placeholder="Tomato Rating" name="tomatoRating" movieState={movieState} setMovieState={setMovieState} />
             </td>
           </tr>
         </tbody>
