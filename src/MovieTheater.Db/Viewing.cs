36f905207b464814b68bda1c25599bf205e0c86d
Neil Bostian <neil.bostian@gmail.com>
2022-07-13 14:10:07 -0400

    fix: compile error

--- src/MovieTheater.Db/Viewing.cs
+++ src/MovieTheater.Db/Viewing.cs
@@ -13,7 +13,7 @@ namespace MovieTheater.Db
         public int MovieID { get; set; }
 
         [ForeignKey(nameof(MovieID))]
-        public Movie Movie { get; set; } = default!
+        public Movie Movie { get; set; } = default!;
 
         public int UserID { get; set; }
 
