805a11918eaaca7518e508e982a369eba4d313f4
Neil Bostian <neil.bostian@gmail.com>
2026-04-11 19:38:42 -0400

    new cluster

--- docker/build-api.sh
+++ docker/build-api.sh
@@ -1,12 +1,18 @@
 
+REGISTRY=${REGISTRY:-registry.local:32500}
+IMAGE_TAG=${IMAGE_TAG:-latest}
+
 # Tar all .csproj files to preserve directory structure
 # https://andrewlock.net/optimising-asp-net-core-apps-in-docker-avoiding-manually-copying-csproj-files/
 find .. -name "*.csproj" -print0 | tar -cvf projectfiles.tar --null -T -
 
 docker build \
-  -t localhost:32000/movietheater-api:$GITHUB_SHA \
-  -t localhost:32000/movietheater-api:latest \
+  --cache-from $REGISTRY/movietheater-api:latest \
+  --build-arg BUILDKIT_INLINE_CACHE=1 \
+  -t $REGISTRY/movietheater-api:$IMAGE_TAG \
   -f Dockerfile.api \
   --network host \
   ..
 
+docker tag $REGISTRY/movietheater-api:$IMAGE_TAG $REGISTRY/movietheater-api:latest
+docker push $REGISTRY/movietheater-api:latest
--- docker/build-ui.sh
+++ docker/build-ui.sh
@@ -1,8 +1,14 @@
 
+REGISTRY=${REGISTRY:-registry.local:32500}
+IMAGE_TAG=${IMAGE_TAG:-latest}
+
 docker build \
-  -t localhost:32000/movietheater-ui:$GITHUB_SHA \
-  -t localhost:32000/movietheater-ui:latest \
+  --cache-from $REGISTRY/movietheater-ui:latest \
+  --build-arg BUILDKIT_INLINE_CACHE=1 \
+  -t $REGISTRY/movietheater-ui:$IMAGE_TAG \
   -f Dockerfile.ui \
   --network host \
   ..
 
+docker tag $REGISTRY/movietheater-ui:$IMAGE_TAG $REGISTRY/movietheater-ui:latest
+docker push $REGISTRY/movietheater-ui:latest
